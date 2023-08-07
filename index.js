cf8250476018fcf5ad219c065e96a72f4ed16521
preciousnjukia <preciousnjukia@gmail.com>
2023-08-08 00:05:17 +0300

    Updates

--- index.js
+++ index.js
@@ -57,4 +57,47 @@ document.addEventListener('DOMContentLoaded', () => {
       likesCount.textContent = likes;
     }
   });
+  document.addEventListener("DOMContentLoaded", () => {
+    const profileForm = document.getElementById("profile-form");
+    const progressCanvas = document.getElementById("progressCanvas");
+
+    profileForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        // Fetch user input and update progress visualization
+        const name = document.getElementById("name").value;
+        const age = parseInt(document.getElementById("age").value);
+        const fitnessGoal = document.getElementById("fitness-goal").value;
+
+        updateProgressVisualization(name, age, fitnessGoal);
+    });
+
+    function updateProgressVisualization(name, age, fitnessGoal) {
+        // Simulated progress data, you can replace this with actual data
+        const progressData = [30, 40, 50, 60, 70, 80, 90, 100];
+
+        // Assuming you use Chart.js library for visualization
+        const progressChart = new Chart(progressCanvas.getContext("2d"), {
+            type: "line",
+            data: {
+                labels: ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8"],
+                datasets: [{
+                    label: `${name}'s Progress`,
+                    data: progressData,
+                    borderColor: "blue",
+                    fill: false,
+                }],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        max: 100,
+                    },
+                },
+            },
+        });
+    }
+});
+
   
